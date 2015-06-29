c3ab2ae4c1862c952477d4f8f1f81036811793a5
Tyler Stark <theirondeveloper@gmail.com>
2015-06-28 17:52:33 -0700

    Setting up the module

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var compress = require('./lib/compress'),
+	expand = require('./lib/expand'),
+	generateNext = require('./lib/generateNext');
+
+module.exports = {
+	compress: compress,
+	expand: expand,
+	generateNext: generateNext
+};
