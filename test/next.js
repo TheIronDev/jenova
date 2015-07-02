2a91c811011701f03c63b6a26611ce3ca23886e9
Tyler Stark <theirondeveloper@gmail.com>
2015-07-02 15:02:13 -0700

    Calculate next game of life board

--- test/next.js
+++ test/next.js
@@ -4,5 +4,14 @@ var assert = require('assert'),
 	next = require('../lib/next');
 
 describe('Next', function() {
-	it('should generate a new game of life array');
+
+	it('should generate a new game of life board', function(done) {
+		var testBoard = [[0,0,0,0,0], [0,1,0,0,0], [0, 1, 1, 0, 0], [0,1,1,0,0], [0,0,0,0,0]],
+			expected = [[0,0,0,0,0],[0,1,1,0,0],[1,0,0,0,0], [0,1,1,0,0], [0,0,0,0,0]];
+
+		next(testBoard, function(result) {
+			assert.deepEqual(result, expected);
+			done();
+		});
+	});
 });
