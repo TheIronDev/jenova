c404fdef31d2b8f84aef86df9692b2685e5b30c6
Tyler Stark <theirondeveloper@gmail.com>
2015-07-04 18:45:56 -0700

    More thorough testing

--- test/next.js
+++ test/next.js
@@ -14,4 +14,80 @@ describe('Next', function() {
 			done();
 		});
 	});
+
+	describe('should follow the game\'s rules', function() {
+		it('starve with 0 neighbors', function(done) {
+			var zeroNeighbors = [[0,0,0,0,0], [0,0,0,0,0], [0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0]];
+
+			next(zeroNeighbors, function(result) {
+				assert.deepEqual(result[2][2], 0);
+				done();
+			});
+		});
+
+		it('starve with 1 neighbor', function(done) {
+			var zeroNeighbors = [[0,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0]];
+
+			next(zeroNeighbors, function(result) {
+				assert.deepEqual(result[2][2], 0);
+				done();
+			});
+		});
+		it('survive with 2 neighbors', function(done) {
+			var zeroNeighbors = [[0,0,0,0,0], [0,1,1,0,0], [0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0]];
+
+			next(zeroNeighbors, function(result) {
+				assert.deepEqual(result[2][2], 1);
+				done();
+			});
+		});
+		it('survive with 3 neighbors', function(done) {
+			var zeroNeighbors = [[0,0,0,0,0], [0,1,1,1,0], [0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0]];
+
+			next(zeroNeighbors, function(result) {
+				assert.deepEqual(result[2][2], 1);
+				done();
+			});
+		});
+		it('starve with 4 neighbors', function(done) {
+			var zeroNeighbors = [[0,0,0,0,0], [0,1,1,1,0], [0,1,1,0,0], [0,0,0,0,0], [0,0,0,0,0]];
+
+			next(zeroNeighbors, function(result) {
+				assert.deepEqual(result[2][2], 0);
+				done();
+			});
+		});
+		it('starve with 5 neighbors', function(done) {
+			var zeroNeighbors = [[0,0,0,0,0], [0,1,1,1,0], [0,1,1,1,0], [0,0,0,0,0], [0,0,0,0,0]];
+
+			next(zeroNeighbors, function(result) {
+				assert.deepEqual(result[2][2], 0);
+				done();
+			});
+		});
+		it('remain dead with 2 neighbors', function(done) {
+			var zeroNeighbors = [[0,0,0,0,0], [0,1,1,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]];
+
+			next(zeroNeighbors, function(result) {
+				assert.deepEqual(result[2][2], 0);
+				done();
+			});
+		});
+		it('repopulate with 3 neighbors', function(done) {
+			var zeroNeighbors = [[0,0,0,0,0], [0,1,1,1,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]];
+
+			next(zeroNeighbors, function(result) {
+				assert.deepEqual(result[2][2], 1);
+				done();
+			});
+		});
+		it('remain dead with 4 neighbors', function(done) {
+			var zeroNeighbors = [[0,0,0,0,0], [0,1,1,0,0], [0,0,0,0,0], [0,1,1,0,0], [0,0,0,0,0]];
+
+			next(zeroNeighbors, function(result) {
+				assert.deepEqual(result[2][2], 0);
+				done();
+			});
+		});
+	});
 });
