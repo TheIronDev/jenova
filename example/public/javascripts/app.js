d51d4ac9c0bab9d0ee0b864f3dfe3c8bd7140d16
Tyler Stark <theirondeveloper@gmail.com>
2016-04-16 17:40:09 -0700

    Wiring up jenova.draw() to our sample app

--- example/public/javascripts/app.js
+++ example/public/javascripts/app.js
@@ -1,2 +1,15 @@
 
-console.log('Bacon is delicious!');
+
+var jenova = require('../../../index.js');
+
+let initialBoard = [
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 1, 0, 0, 0, 0, 0],
+	[0, 0, 1, 1, 0, 0, 0],
+	[0, 1, 1, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0]
+];
+
+jenova.draw('calamity', initialBoard);
